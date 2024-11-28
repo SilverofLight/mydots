d81417a0cc524aaf139a9e57a2c32526a3661fbe
SilverofLight <a2061686759@outlook.com>
2024-11-28 22:45:51 +0800

    exit menu

---
+++ hypr/.config/hypr/scripts/exit.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 定义菜单选项
+options=" ⏻ Shutdown\n  Reboot\n 󰿅 Logout\n 󰒲 Suspend\n 󰌾 Lock"
+
+# 使用 rofi 提示选择
+chosen=$(echo -e "$options" | rofi -dmenu -i -p "Power options:")
+
+# 根据选择执行相应的命令
+case $chosen in
+    " ⏻ Shutdown")
+        systemctl poweroff -i
+        ;;
+    "  Reboot")
+        systemctl reboot
+        ;;
+    " 󰿅 Logout")
+        hyprtcl dispatch exit
+        ;;
+    " 󰒲 Suspend")
+        systemctl suspend
+        ;;
+    " 󰌾 Lock")
+        swaylock
+        ;;
+    *)
+        exit 0
+        ;;
+esac
