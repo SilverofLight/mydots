d3ed3e95281f8869f54e98747142c808a97434ca
SilverofLight <a2061686759@outlook.com>
2024-12-01 15:02:27 +0800

    recording sign

--- hypr/.config/hypr/scripts/recorder.sh
+++ hypr/.config/hypr/scripts/recorder.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-monitors=$(hyprctl monitors all | grep Monitor | awk '{print $2}')
+# 检查是否已经在录制
+if pgrep -x "wf-recorder" > /dev/null; then
+    # 如果正在录制，则停止录制
+    killall -s SIGINT wf-recorder
+    pkill -RTMIN+8 waybar
+    notify-send "录制结束" "视频已保存"
+    exit 0
+fi
 
-# wf-recorder -f $(xdg-user-dir Videos)/$(date +'%H:%M:%S_%d-%m-%Y.mp4')
-# killall -s SIGINT wf-recorder
+monitors=$(hyprctl monitors all | grep Monitor | awk '{print $2}')
 
 chosen=$(echo -e "$monitors" | rofi -dmenu -i -p "Select a Monitor:")
 
@@ -14,9 +20,7 @@ case $chosen in
         ;;
     *): 
         echo "Selected monitor: $chosen"
-        wf-recorder -a -f $(xdg-user-dir Videos)/$(date +'%H:%M:%S_%d-%m-%Y').mp4 -o $chosen
-        ;;
-    *)
-        echo "Invalid option"
+        wf-recorder -a -f $HOME/Videos/$(date +'%H:%M:%S_%d-%m-%Y').mp4 -o $chosen
+		pkill -RTMIN+8 waybar
         ;;
 esac
