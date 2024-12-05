08d41a8d0cea261024c2e956c06d3c5f2e97c530
SilverofLight <a2061686759@outlook.com>
2024-12-05 17:28:58 +0800

    fix

--- waybar/.config/waybar/scripts/get_proxy.sh
+++ waybar/.config/waybar/scripts/get_proxy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s -X GET 'http://127.0.0.1:9090/proxies' -H 'Authorization: Bearer woshi1gg' | jq | grep "now" | awk 'NR==3'| sed 's/.*: "\(.*\)",/\1/'
+curl -s -X GET 'http://127.0.0.1:9090/proxies' -H 'Authorization: Bearer woshi1gg' | jq | grep "now" | awk 'NR==12'| sed 's/.*: "\(.*\)",/\1/'
