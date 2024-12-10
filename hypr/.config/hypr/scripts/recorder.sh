6b42042f1150206a95ac08ef9ae29d34b443eafa
SilverofLight <a2061686759@outlook.com>
2024-12-10 19:18:45 +0800

    disable double input

--- hypr/.config/hypr/scripts/recorder.sh
+++ hypr/.config/hypr/scripts/recorder.sh
@@ -5,15 +5,12 @@ if pgrep -x "wf-recorder" > /dev/null; then
     pkill -RTMIN+8 waybar
     killall -s SIGINT wf-recorder
     pkill -RTMIN+8 waybar
-    pactl unload-module module-loopback
-    pactl unload-module module-null-sink
-    pkill -RTMIN+8 waybar
     notify-send "Recording Finished" "The video is saved"
     exit 0
 fi
 
 monitors=$(hyprctl monitors all | grep Monitor | awk '{print $2}')
-# audio_sources=$(pactl list sources | grep Name | cut -d: -f2 | sed 's/^[[:space:]]*//')
+audio_sources=$(pactl list sources | grep Name | cut -d: -f2 | sed 's/^[[:space:]]*//')
 
 chosen_monitor=$(echo -e "$monitors" | rofi -dmenu -i -mesg "Select Monitor")
 if [ -z "$chosen_monitor" ]; then
@@ -21,38 +18,12 @@ if [ -z "$chosen_monitor" ]; then
     exit 1
 fi
 
-output_audios=$(pactl list sources | grep "output" | grep Name | awk '{print $2}')
-chosen_output=$(echo -e "No output\n$output_audios" | rofi -dmenu -i -mesg "Select output audio")
+# 选择音频源
+chosen_audio=$(echo -e "$audio_sources\nno audio" | rofi -dmenu -i -mesg "Select Audio")
 
-if [ -z "$chosen_output" ]; then
-    notify-send "Output Cancelled" "No output selected"
+if [ -z "$chosen_audio" ]; then
+    notify-send "Recording Cancelled" "No audio selected"
     exit 1
-fi
-
-input_audios=$(pactl list sources | grep "input" | grep Name | awk '{print $2}')
-chosen_input=$(echo -e "No input\n$input_audios" | rofi -dmenu -i -mesg "Select input")
-
-if [ -z "$chosen_input" ]; then
-    notify-send "Input Cancelled" "No input selected"
-    exit 1
-fi
-
-if [ "$chosen_output" == "No output" ]; then
-    if [ "$chosen_input" == "No input" ]; then
-        chosen_audio=""
-    else
-        chosen_audio="$chosen_input"
-    fi
-else
-    if [ "$chosen_input" == "No input" ]; then
-        chosen_audio="$chosen_output"
-    else
-        pactl load-module module-null-sink sink_name=Combined
-        pactl load-module module-loopback sink=Combined source=$chosen_output
-        pactl load-module module-loopback sink=Combined source=$chosen_input
-        sleep 1
-        chosen_audio="Combined.monitor"
-    fi
 fi
 
 chosen_format=$(echo -e "mkv\nmp4" | rofi -dmenu -i -mesg "Choose format")
@@ -64,7 +35,7 @@ fi
 
 record_cmd="wf-recorder"
 
-if [ -n "$chosen_audio" ]; then
+if [ "$chosen_audio" != "no audio" ] && [ -n "$chosen_audio" ]; then
     record_cmd+=" --audio=$chosen_audio"
 fi
 
--- hypr/.config/hypr/scripts/recorder_back.sh
+++ hypr/.config/hypr/scripts/recorder_back.sh
@@ -5,12 +5,15 @@ if pgrep -x "wf-recorder" > /dev/null; then
     pkill -RTMIN+8 waybar
     killall -s SIGINT wf-recorder
     pkill -RTMIN+8 waybar
+    pactl unload-module module-loopback
+    pactl unload-module module-null-sink
+    pkill -RTMIN+8 waybar
     notify-send "Recording Finished" "The video is saved"
     exit 0
 fi
 
 monitors=$(hyprctl monitors all | grep Monitor | awk '{print $2}')
-audio_sources=$(pactl list sources | grep Name | cut -d: -f2 | sed 's/^[[:space:]]*//')
+# audio_sources=$(pactl list sources | grep Name | cut -d: -f2 | sed 's/^[[:space:]]*//')
 
 chosen_monitor=$(echo -e "$monitors" | rofi -dmenu -i -mesg "Select Monitor")
 if [ -z "$chosen_monitor" ]; then
@@ -18,24 +21,57 @@ if [ -z "$chosen_monitor" ]; then
     exit 1
 fi
 
-# 选择音频源
-chosen_audio=$(echo -e "$audio_sources\nno audio" | rofi -dmenu -i -mesg "Select Audio")
+output_audios=$(pactl list sources | grep "output" | grep Name | awk '{print $2}')
+chosen_output=$(echo -e "No output\n$output_audios" | rofi -dmenu -i -mesg "Select output audio")
 
-if [ -z "$chosen_audio" ]; then
-    notify-send "Recording Cancelled" "No audio selected"
+if [ -z "$chosen_output" ]; then
+    notify-send "Output Cancelled" "No output selected"
+    exit 1
+fi
+
+input_audios=$(pactl list sources | grep "input" | grep Name | awk '{print $2}')
+chosen_input=$(echo -e "No input\n$input_audios" | rofi -dmenu -i -mesg "Select input")
+
+if [ -z "$chosen_input" ]; then
+    notify-send "Input Cancelled" "No input selected"
+    exit 1
+fi
+
+if [ "$chosen_output" == "No output" ]; then
+    if [ "$chosen_input" == "No input" ]; then
+        chosen_audio=""
+    else
+        chosen_audio="$chosen_input"
+    fi
+else
+    if [ "$chosen_input" == "No input" ]; then
+        chosen_audio="$chosen_output"
+    else
+        pactl load-module module-null-sink sink_name=Combined
+        pactl load-module module-loopback sink=Combined source=$chosen_output
+        pactl load-module module-loopback sink=Combined source=$chosen_input
+        sleep 1
+        chosen_audio="Combined.monitor"
+    fi
+fi
+
+chosen_format=$(echo -e "mkv\nmp4" | rofi -dmenu -i -mesg "Choose format")
+
+if [ -z "chosen_format" ]; then
+    notify-send "Format Cancelled" "No format selected"
     exit 1
 fi
 
 record_cmd="wf-recorder"
 
-if [ "$chosen_audio" != "no audio" ] && [ -n "$chosen_audio" ]; then
+if [ -n "$chosen_audio" ]; then
     record_cmd+=" --audio=$chosen_audio"
 fi
 
 sleep 2
 
 pkill -RTMIN+8 waybar
-$record_cmd -f "$HOME/Videos/$(date +'%H:%M:%S_%d-%m-%Y').mp4" -o "$chosen_monitor" &
+$record_cmd -f "$HOME/Videos/$(date +'%H:%M:%S_%d-%m-%Y').$chosen_format" -o "$chosen_monitor" &
 pkill -RTMIN+8 waybar
 
 # notify-send "Recording Started" "Monitor: $chosen_monitor\nAudio: ${chosen_audio:-no audio}"
