65c4f53484176465a1822dc0742b625d087153e3
SilverofLight <a2061686759@outlook.com>
2025-01-30 13:13:42 +0800

    hypr: nvim anywhere

---
+++ hypr/.config/hypr/scripts/nvimAnywhere.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# author: Silver Lee
+# date: 2025/1/30
+# description: use nvim to edit a temporary file and copy the content to clipboard
+# requirements: kitty, nvim, wl-copy || xclip || pbcopy
+
+temp_file=$(mktemp /tmp/nvimAnywhere.XXXXXX)
+
+kitty -T nvimAnywhere fish -c "nvim $temp_file"
+
+if command -v wl-copy &> /dev/null; then
+    cat "$temp_file" | wl-copy
+elif command -v xclip &> /dev/null; then
+    cat "$temp_file" | xclip -selection clipboard
+elif command -v pbcopy &> /dev/null; then
+    cat "$temp_file" | pbcopy
+else
+    echo "未找到 wl-copy, xclip 或 pbcopy，无法复制到剪切板"
+fi
+
+rm "$temp_file"
