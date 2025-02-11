4048b727a445b1ff655842e7a271f5a969f83fac
SilverofLight <a2061686759@outlook.com>
2025-02-11 22:01:28 +0800

    hypr: dvorak

---
+++ hypr_dvorak/.config/hypr/scripts/cliphist.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+# roconf="~/.config/rofi/clipboard.rasi"
+roconf="~/.config/rofi/config.rasi"
+
+case $1 in
+    c)  cliphist list | rofi -dmenu -theme-str 'entry { placeholder: "Copy...";}' -config $roconf -mesg 'Clipboard' | cliphist decode | wl-copy
+        ;; 
+    d)  cliphist list | rofi -dmenu -theme-str 'entry { placeholder: "Delete...";}' -config $roconf | cliphist delete
+        ;;
+    w)  if [ `echo -e "Yes\nNo" | rofi -dmenu -theme-str 'entry { placeholder: "Clear Clipboard History?";}' -config $roconf` == "Yes" ] ; then
+            cliphist wipe
+        fi
+        ;;
+    t)  echo ""
+        echo "󰅇 clipboard history"
+        ;;
+    *)  echo "cliphist.sh [action]"
+        echo "c :  cliphist list and copy selected"
+        echo "d :  cliphist list and delete selected"
+        echo "w :  cliphist wipe database"
+        echo "t :  display tooltip"
+        ;;
+esac
+
---
+++ hypr_dvorak/.config/hypr/scripts/moveto.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# author: Silver Lee
+# date: 2025/1/5
+# description: move a window to specific workspace
+# requirements: rofi, hyprland
+
+# workspaces=$(hyprctl workspaces | grep "workspace ID" | awk -F'[()]' '{print $2}')
+
+workspaces=$(hyprctl workspaces | grep "workspace ID" | awk '{print $2,$3,$4,$5,$6,$7}')
+
+chosen_workspace=$(echo -e "$workspaces\nCreateNew" | rofi -dmenu -i -msg "Move to workspace")
+
+# echo $chosen_workspace
+
+if [ -z "$chosen_workspace" ]; then
+  notify-send "Move window canceled" "No workspace selected"
+  exit 1
+fi
+
+if [ "$chosen_workspace" = "CreateNew" ]; then
+    # 弹出输入框让用户输入新workspace的名称
+    new_name=$(rofi -dmenu -mesg "Enter new Workspace name")
+    if [ -n "$new_name" ]; then
+        # 创建并切换到新的special workspace
+        hyprctl dispatch movetoworkspace "special:$new_name"
+    fi
+    exit 1
+fi
+
+spaceID=$(echo $chosen_workspace | awk -F'[()]' '{print $2}')
+
+# echo $spaceID
+
+hyprctl dispatch movetoworkspace $spaceID
