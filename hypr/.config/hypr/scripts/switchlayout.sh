d10719ab587bee696dc456dd8328ee055af60f6d
SilverofLight <a2061686759@outlook.com>
2025-02-13 17:09:54 +0800

    dvorak -> colemak

--- hypr/.config/hypr/scripts/switchlayout.sh
+++ hypr/.config/hypr/scripts/switchlayout.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-IsDvorak=$(hyprctl devices | grep Dvorak | wc -l)
+# IsDvorak=$(hyprctl devices | grep Dvorak | wc -l)
+#
+# if [ $IsDvorak == 0 ]; then
+#     notify-send "Switching to Dvorak"
+#     hyprctl keyword input:kb_variant "dvorak"
+# else
+#     notify-send "Switching to Qwerty"
+#     hyprctl keyword input:kb_variant ""
+# fi
 
-if [ $IsDvorak == 0 ]; then
-    notify-send "Switching to Dvorak"
-    hyprctl keyword input:kb_variant "dvorak"
+IsColemak=$(hyprctl devices | grep Colemak | wc -l)
+
+if [ $IsColemak == 0 ]; then
+    notify-send "Switching to Colemak"
+    hyprctl keyword input:kb_variant "colemak"
 else
     notify-send "Switching to Qwerty"
     hyprctl keyword input:kb_variant ""
