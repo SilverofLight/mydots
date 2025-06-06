e4e1bc83dd8f36668e9874b67e7dc925d3d39017
SilverofLight <a2061686759@outlook.com>
2025-06-06 21:58:30 +0800

    bv: use wl-paste

--- scripts/.scripts/bv.sh
+++ scripts/.scripts/bv.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-bv=$(wofi --dmenu --prompt "Bv")
+# bv=$(wofi --dmenu --prompt "Bv")
+bv=$(wl-paste)
+echo $bv
 
-mpv --quiet --no-terminal --title="mpv-bilibili" https://www.bilibili.com/video/$bv
+if [[ "$bv" == "BV"* ]]; then
+  notify-send "BV" "Opening $bv"
+  mpv --quiet --no-terminal --title="mpv-bilibili" https://www.bilibili.com/video/$bv
+elif [[ "$bv" == "https://www.bilibili.com/video/BV"* ]]; then
+  notify-send "BV" "Opening $bv"
+  mpv --quiet --no-terminal --title="mpv-bilibili" $bv
+else
+  notify-send "BV" "Not a BV or Url"
+fi
