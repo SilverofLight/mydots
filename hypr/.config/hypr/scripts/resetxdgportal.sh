2da08c8d5ff0c602a8aae83039f17c1afd1aadb4
SilverofLight <a2061686759@outlook.com>
2025-06-20 11:54:53 +0800

    mpv: bg

--- hypr/.config/hypr/scripts/resetxdgportal.sh
+++ hypr/.config/hypr/scripts/resetxdgportal.sh
@@ -5,6 +5,7 @@ killall xdg-desktop-portal-gnome
 killall xdg-desktop-portal-kde
 killall xdg-desktop-portal-lxqt
 killall xdg-desktop-portal-wlr
+killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
 sleep 1
 /usr/lib/xdg-desktop-portal-hyprland &
