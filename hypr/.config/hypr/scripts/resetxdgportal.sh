5301afa32cffb58b24a0ade882d45affd7f5f05c
SilverofLight <a2061686759@outlook.com>
2025-06-24 16:21:38 +0800

    resetxdgportal: add gtk portal launch with adjusted timing

--- hypr/.config/hypr/scripts/resetxdgportal.sh
+++ hypr/.config/hypr/scripts/resetxdgportal.sh
@@ -9,5 +9,7 @@ killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
 sleep 1
 /usr/lib/xdg-desktop-portal-hyprland &
+sleep 1
+/usr/lib/xdg-desktop-portal-gtk &
 sleep 2
 /usr/lib/xdg-desktop-portal &
