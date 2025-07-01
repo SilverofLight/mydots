9a75b32d82df31254b971d00c4f55476c9455871
SilverofLight <a2061686759@outlook.com>
2025-07-01 17:13:48 +0800

    mpv: osd-msg; move ipc to mpv.config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -90,7 +90,6 @@ if status is-interactive
     alias cdd="cd /run/media/silver/"
     alias hibernate="systemctl hibernate"
     alias a="task"
-    alias mpv="mpv --input-ipc-server=/tmp/mpvsocket"
 
     # 如果在使用 kitty, 则更改 ssh
     if test "$TERM" = xterm-kitty
