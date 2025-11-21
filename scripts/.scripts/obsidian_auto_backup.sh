1471592b46843ba4a2934c9f7fea91fdc6ca4576
SilverofLight <a2061686759@outlook.com>
2025-11-21 23:37:39 +0800

    obsidian_auto_backup

---
+++ scripts/.scripts/obsidian_auto_backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~/Study/Obsidian/
+date=$(date +%Y-%m-%d-%H-%M-%S)
+
+if [ -n "$(git status -s)" ]; then
+  echo "Backuping..."
+  git add .
+  git commit -m "Obsidian Backup: $date"
+fi
