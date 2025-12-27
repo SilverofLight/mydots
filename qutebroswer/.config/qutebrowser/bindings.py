8212cf192c5295b0d4593e7e481d545c66a60713
SilverofLight <a2061686759@outlook.com>
2025-12-27 16:36:10 +0800

    qutebroswer

---
+++ qutebroswer/.config/qutebrowser/bindings.py
@@ -0,0 +1,123 @@
+# caret:
+
+config.unbind('J', mode='caret')
+config.unbind('K', mode='caret')
+config.unbind('L', mode='caret')
+
+config.bind('H', 'scroll left', mode='caret')
+config.bind('N', 'scroll down', mode='caret')
+config.bind('E', 'scroll up', mode='caret')
+config.bind('I', 'scroll right', mode='caret')
+
+
+config.unbind('j', mode='caret')
+config.unbind('k', mode='caret')
+config.unbind('l', mode='caret')
+
+config.bind('h', 'move-to-prev-char', mode='caret')
+config.bind('n', 'move-to-next-line', mode='caret')
+config.bind('e', 'move-to-prev-line', mode='caret')
+config.bind('i', 'move-to-next-char', mode='caret')
+
+config.bind('qt', 'spawn --userscript qute-dict', mode='caret')
+
+# normal:
+
+config.unbind('q', mode='normal')
+config.bind('qt', 'spawn --userscript qute-dict')
+config.bind('qh', 'spawn --userscript qute-html')
+
+config.unbind(';I', mode='normal')
+config.unbind(';O', mode='normal')
+config.unbind(';R', mode='normal')
+config.unbind(';Y', mode='normal')
+config.unbind(';b', mode='normal')
+config.unbind(';d', mode='normal')
+config.unbind(';f', mode='normal')
+config.unbind(';h', mode='normal')
+config.unbind(';i', mode='normal')
+config.unbind(';o', mode='normal')
+config.unbind(';r', mode='normal')
+config.unbind(';t', mode='normal')
+config.unbind(';y', mode='normal')
+config.bind(';', 'cmd-set-text :', mode='normal')
+
+
+config.unbind('<Alt-1>', mode='normal')
+config.unbind('<Alt-2>', mode='normal')
+config.unbind('<Alt-3>', mode='normal')
+config.unbind('<Alt-4>', mode='normal')
+config.unbind('<Alt-5>', mode='normal')
+config.unbind('<Alt-6>', mode='normal')
+config.unbind('<Alt-7>', mode='normal')
+config.unbind('<Alt-8>', mode='normal')
+config.unbind('<Alt-9>', mode='normal')
+config.unbind('<Alt-m>', mode='normal')
+config.bind('<Meta-1>', "tab-focus 1", mode='normal')
+config.bind('<Meta-2>', "tab-focus 2", mode='normal')
+config.bind('<Meta-3>', "tab-focus 3", mode='normal')
+config.bind('<Meta-4>', "tab-focus 4", mode='normal')
+config.bind('<Meta-5>', "tab-focus 5", mode='normal')
+config.bind('<Meta-6>', "tab-focus 6", mode='normal')
+config.bind('<Meta-7>', "tab-focus 7", mode='normal')
+config.bind('<Meta-8>', "tab-focus 8", mode='normal')
+config.bind('<Meta-9>', "tab-focus 9", mode='normal')
+config.bind('<Meta-m>', "tab-mute", mode='normal')
+
+
+config.bind('x', "tab-close", mode='normal')
+config.bind('H', "back", mode='normal')
+config.bind('N', "tab-next", mode='normal')
+config.bind('E', "tab-prev", mode='normal')
+config.bind('I', "forward", mode='normal')
+config.bind('K', "search-prev", mode='normal')
+
+config.bind('s', 'hint all tab-fg', mode='normal')
+config.bind('L', 'undo -w', mode='normal')
+
+config.bind('d', 'scroll-page 0 0.5', mode='normal')
+config.bind('l', 'scroll-page 0 -0.5', mode='normal')
+
+config.bind('h', 'scroll-page -0.1 0', mode='normal')
+config.bind('n', 'scroll-page 0 0.1', mode='normal')
+config.bind('e', 'scroll-page 0 -0.1', mode='normal')
+config.bind('i', 'scroll-page 0.1 0', mode='normal')
+
+config.bind('m', 'mode-enter set_mark', mode='normal')
+config.bind('k', 'search-next', mode='normal')
+
+config.unbind('sf', mode='normal')
+config.unbind('sk', mode='normal')
+config.unbind('sl', mode='normal')
+config.unbind('ss', mode='normal')
+
+config.bind('Q', "macro-record", mode='normal')
+config.bind('t', 'open -t', mode='normal')
+
+config.unbind('u', mode='normal')
+
+config.bind('u', 'mode-enter insert', mode="normal")
+
+config.bind('.', 'config-cycle tabs.show always never')
+
+config.unbind('g$', mode="normal")
+config.unbind('g0', mode="normal")
+config.unbind('gB', mode="normal")
+config.unbind('gO', mode="normal")
+config.unbind('gd', mode="normal")
+config.unbind('gm', mode="normal")
+config.unbind('go', mode="normal")
+config.unbind('gu', mode="normal")
+config.unbind('ga', mode="normal")
+config.unbind('gb', mode="normal")
+config.unbind('g^', mode="normal")
+config.unbind('gU', mode="normal")
+
+config.bind('guu', 'spawn -u qute-bw username', mode="normal")
+config.bind('gup', 'spawn -u qute-bw password', mode="normal")
+config.bind('guf', 'spawn -u qute-bw fill', mode="normal")
+config.bind('b', 'spawn -u qute-bookmarks open', mode='normal')
+config.bind('B', 'spawn -u qute-bookmarks add', mode='normal')
+
+# hint
+config.bind('s', 'mode-leave', mode="hint")
---
+++ qutebroswer/.config/qutebrowser/config.py
@@ -0,0 +1,70 @@
+from dracula import blood
+
+config.load_autoconfig(False)
+# c.content.user_stylesheets = ["~/.config/qutebrowser/styles/black.css"]
+
+blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
+
+config.source('bindings.py')
+
+# NOTE: file chooser (untested)
+fileChooser = ['ketty', '-T', 'Floating_Term', '-c', 'yazi', '--chooser-file "$1"', '--', '{}']
+c.fileselect.handler = "external"
+c.fileselect.folder.command = fileChooser
+c.fileselect.multiple_files.command = fileChooser
+c.fileselect.single_file.command = fileChooser
+
+c.zoom.levels = ["25%", "33%", "50%", "67%", "75%", "90%", "100%", "110%", "120%", "130%", "140%", "150%", "175%", "200%", "250%", "300%"]
+
+# c.url.start_pages = 'file:///dev/null'
+# c.url.default_page= 'file:///dev/null'
+# c.tabs.last_close = "startpage"
+c.auto_save.session = False
+
+
+c.tabs.show = "always"
+c.tabs.position = "right"
+c.tabs.padding = {"bottom":0, "left":0, "right":0, "top":0}
+c.tabs.indicator.width = 0
+c.tabs.width = '7%'
+c.statusbar.show = "always"
+c.completion.height = '30%'
+
+c.keyhint.delay = 0
+c.hints.uppercase = False
+c.hints.chars = "artdhneio"
+
+c.downloads.location.directory = "~/Downloads/"
+c.downloads.location.prompt = False
+c.downloads.location.suggestion = 'both'
+c.downloads.location.remember = False
+c.downloads.remove_finished = 3300
+c.downloads.position = "bottom"
+
+c.content.fullscreen.window = True # Limit fullscreen to browser window
+c.content.blocking.enabled = True
+c.content.blocking.method = 'both'
+c.content.blocking.adblock.lists = [
+  "https://easylist.to/easylist/easylist.txt",
+  "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
+  "https://easylist.to/easylist/easyprivacy.txt",
+  "https://secure.fanboy.co.nz/fanboy-annoyance.txt",]
+
+c.scrolling.smooth = True
+c.content.autoplay = True
+
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.policy.images = 'never'
+c.colors.webpage.bg = '#282a36' # fix darkmode white flash
+
+# c.content.headers.user_agent = "Mozilla/5.0 ({os_info}; rv:135.0) Gecko/20100101 Firefox/135"
+config.set(
+    'content.headers.user_agent',
+    'Mozilla/5.0 ({os_info}; rv:135.0) Gecko/20100101 Firefox/135',
+    pattern='https://accounts.google.com/*'
+)
