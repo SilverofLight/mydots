0df989ba7b39c9ef786bf08bb5e3d573fb98b21f
SilverofLight <a2061686759@outlook.com>
2025-12-27 18:16:20 +0800

    qutebroswer use yazi as file chooser

--- qutebroswer/.config/qutebrowser/config.py
+++ qutebroswer/.config/qutebrowser/config.py
@@ -13,7 +13,7 @@ blood(c, {
 config.source('bindings.py')
 
 # NOTE: file chooser (untested)
-fileChooser = ['ketty', '-T', 'Floating_Term', '-c', 'yazi', '--chooser-file "$1"', '--', '{}']
+fileChooser = ['kitty', '-T', 'Floating_Term', '-e', 'fish', '-c', 'yazi --chooser-file="$argv[1]"', '--', '{}']
 c.fileselect.handler = "external"
 c.fileselect.folder.command = fileChooser
 c.fileselect.multiple_files.command = fileChooser
@@ -37,7 +37,7 @@ c.completion.height = '30%'
 
 c.keyhint.delay = 0
 c.hints.uppercase = False
-c.hints.chars = "artdhneio"
+c.hints.chars = "artdhneioplvmcx"
 
 c.downloads.location.directory = "~/Downloads/"
 c.downloads.location.prompt = False
