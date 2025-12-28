c9830d4b0344f5bafc53d56a3ee6d538f6e1e3f4
SilverofLight <a2061686759@outlook.com>
2025-12-28 09:21:25 +0800

    hint.js: add deepseek

--- qutebroswer/.config/qutebrowser/greasemonkey/hint.js
+++ qutebroswer/.config/qutebrowser/greasemonkey/hint.js
@@ -4,8 +4,23 @@
     const old_add_event_listener = Element.prototype.addEventListener;
     Element.prototype.addEventListener = function() {
         if (arguments[0] === 'click') {
-            this.classList.add('.qutebrowser-custom-click');
+            this.classList.add('qutebrowser-custom-click');
         }
-        return old_add_event_listener.apply(this, arguments)
+        return old_add_event_listener.apply(this, arguments);
+    };
+
+    const host = window.location.host;
+
+    if (host === 'chat.deepseek.com') {
+        window.addEventListener('load', function() {
+            let target = "开启新对话";
+            let walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null);
+            let node;
+            while (node = walker.nextNode()) {
+                if (node.nodeType === Node.TEXT_NODE && node.nodeValue === target) {
+                    node.parentNode.classList.add('qutebrowser-custom-click');
+                }
+            }
+        });
     }
-})()
+})();
