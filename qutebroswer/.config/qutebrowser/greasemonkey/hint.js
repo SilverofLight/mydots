a6dc72dc470e5cbdf0ed00bf6960a9a4021178df
SilverofLight <a2061686759@outlook.com>
2026-01-02 19:20:50 +0800

    qute: clear msg after ad

--- qutebroswer/.config/qutebrowser/greasemonkey/hint.js
+++ qutebroswer/.config/qutebrowser/greasemonkey/hint.js
@@ -1,6 +1,14 @@
 (function() {
     'use strict';
 
+    // const old_add_event_listener = Element.prototype.addEventListener;
+    // Element.prototype.addEventListener = function() {
+    //     if (arguments[0] === 'click') {
+    //         this.classList.add('qutebrowser-custom-click');
+    //     }
+    //     return old_add_event_listener.apply(this, arguments);
+    // };
+    //
     const host = window.location.host;
 
     if (host === 'chat.deepseek.com') {
