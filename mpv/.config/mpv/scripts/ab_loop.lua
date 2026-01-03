0196c3689461460d148dc18f2a89fdaa87aadb18
SilverofLight <a2061686759@outlook.com>
2026-01-03 16:54:14 +0800

    mpv: like potplayer loop

--- mpv/.config/mpv/scripts/ab_loop.lua
+++ mpv/.config/mpv/scripts/ab_loop.lua
@@ -1,21 +1,21 @@
 local mp = require("mp")
 
 -- 设置 ab-loop-a
-function set_a()
+local function set_a()
 	local current_pos = mp.get_property_number("time-pos")
 	mp.set_property_number("ab-loop-a", current_pos)
 	mp.osd_message(string.format("Set ab-loop-a to %.2f", current_pos))
 end
 
 -- 设置 ab-loop-b
-function set_b()
+local function set_b()
 	local current_pos = mp.get_property_number("time-pos")
 	mp.set_property_number("ab-loop-b", current_pos)
 	mp.osd_message(string.format("Set ab-loop-b to %.2f", current_pos))
 end
 
 -- 清除 ab-loop-a
-function clear_a()
+local function clear_a()
 	local pos = mp.get_property_number("ab-loop-a")
 	if pos then
 		mp.set_property("ab-loop-a", "no")
@@ -26,7 +26,7 @@ function clear_a()
 end
 
 -- 清除 ab-loop-b
-function clear_b()
+local function clear_b()
 	local pos = mp.get_property_number("ab-loop-b")
 	if pos then
 		mp.set_property("ab-loop-b", "no")
@@ -37,7 +37,7 @@ function clear_b()
 end
 
 -- 回到 ab-loop-a 的位置
-function seek_to_a()
+local function seek_to_a()
 	local pos = mp.get_property_number("ab-loop-a")
 	if pos then
 		mp.set_property_number("time-pos", pos)
@@ -47,9 +47,35 @@ function seek_to_a()
 	end
 end
 
+local function set_or_clear_a()
+	local mark_pos = mp.get_property_number("ab-loop-a")
+	if mark_pos then
+		mp.set_property("ab-loop-a", "no")
+		mp.osd_message("Cleared ab-loop-a")
+	else
+		local current_pos = mp.get_property_number("time-pos")
+		mp.set_property_number("ab-loop-a", current_pos)
+		mp.osd_message(string.format("Set ab-loop-a to %.2f", current_pos))
+	end
+end
+
+local function set_or_clear_b()
+	local mark_pos = mp.get_property_number("ab-loop-b")
+	if mark_pos then
+		mp.set_property("ab-loop-b", "no")
+		mp.osd_message("Cleared ab-loop-b")
+	else
+		local current_pos = mp.get_property_number("time-pos")
+		mp.set_property_number("ab-loop-b", current_pos)
+		mp.osd_message(string.format("Set ab-loop-b to %.2f", current_pos))
+	end
+end
+
 -- 绑定按键
-mp.add_key_binding("down", "set-a", set_a)
-mp.add_key_binding("up", "set-b", set_b)
-mp.add_key_binding("left", "clear-a", clear_a)
-mp.add_key_binding("right", "clear-b", clear_b)
+-- mp.add_key_binding("down", "set-a", set_a)
+-- mp.add_key_binding("up", "set-b", set_b)
+-- mp.add_key_binding("left", "clear-a", clear_a)
+-- mp.add_key_binding("right", "clear-b", clear_b)
 mp.add_key_binding("BS", "seek-to-a", seek_to_a)
+mp.add_key_binding("[", "set-or-clear-a", set_or_clear_a)
+mp.add_key_binding("]", "set-or-clear-b", set_or_clear_b)
