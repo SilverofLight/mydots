d9d0fe9b3f85da870bd5d5910e5f85c2aa65c37c
SilverofLight <a2061686759@outlook.com>
2026-01-03 22:14:15 +0800

    mpv: fix loop

--- mpv/.config/mpv/scripts/ab_loop.lua
+++ mpv/.config/mpv/scripts/ab_loop.lua
@@ -47,35 +47,9 @@ local function seek_to_a()
 	end
 end
 
-local function set_or_clear_a()
-	local mark_pos = mp.get_property_number("ab-loop-a")
-	if mark_pos then
-		mp.set_property("ab-loop-a", "no")
-		mp.osd_message("Cleared ab-loop-a")
-	else
-		local current_pos = mp.get_property_number("time-pos")
-		mp.set_property_number("ab-loop-a", current_pos)
-		mp.osd_message(string.format("Set ab-loop-a to %.2f", current_pos))
-	end
-end
-
-local function set_or_clear_b()
-	local mark_pos = mp.get_property_number("ab-loop-b")
-	if mark_pos then
-		mp.set_property("ab-loop-b", "no")
-		mp.osd_message("Cleared ab-loop-b")
-	else
-		local current_pos = mp.get_property_number("time-pos")
-		mp.set_property_number("ab-loop-b", current_pos)
-		mp.osd_message(string.format("Set ab-loop-b to %.2f", current_pos))
-	end
-end
-
 -- 绑定按键
--- mp.add_key_binding("down", "set-a", set_a)
--- mp.add_key_binding("up", "set-b", set_b)
--- mp.add_key_binding("left", "clear-a", clear_a)
--- mp.add_key_binding("right", "clear-b", clear_b)
 mp.add_key_binding("BS", "seek-to-a", seek_to_a)
-mp.add_key_binding("[", "set-or-clear-a", set_or_clear_a)
-mp.add_key_binding("]", "set-or-clear-b", set_or_clear_b)
+mp.add_key_binding("[", "set-a", set_a)
+mp.add_key_binding("{", "clear-a", clear_a)
+mp.add_key_binding("]", "set-b", set_b)
+mp.add_key_binding("}", "clear-b", clear_b)
