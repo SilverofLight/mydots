d8fadadfcba425f5543232bdac9126f4970c0e1b
SilverofLight <a2061686759@outlook.com>
2026-02-01 22:11:12 +0800

    fix

--- mango/.config/mango/scritps/websearch.sh
+++ mango/.config/mango/scritps/websearch.sh
@@ -30,7 +30,7 @@ fi
 text=$(echo -e "$text" | wofi --conf $HOME/.config/wofi/config_search --dmenu --prompt "search history")
 
 # command="env GTK_IM_MODULE=fcitx,QT_IM_MODULE=fcitx,XMODIFIERS=@im=fcitx /opt/zen-browser-bin/zen-bin"
-command="QT_QPA_PLATFORM=xcb qutebrowser"
+command="env QT_QPA_PLATFORM=xcb; qutebrowser"
 # command=$browser
 
 if [ -z $text ]; then
