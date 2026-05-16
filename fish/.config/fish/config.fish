8ce1f7aa6fab0ac63a117ce9f2ec040d8c965ad4
SilverofLight <a2061686759@outlook.com>
2026-05-16 17:51:25 +0800

    fish: new config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,25 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    # function fish_prompt
-    #   echo -n '@'':'(pwd)\n'>'' '
-    # end
-    # if test (xmodmap -pke | grep -c 'Control_L') -eq 1
-    #     xmodmap $HOME/.Xmodmap
-    #     echo "change keyboard layout successfully"
-    # end
-
-    # ptyhon path
-    # set -gx PATH $HOME/.pyenv/bin $PATH
-
-    # INFO: 尝试禁用核显
-    set -gx DRI_PRIME 1
-
-    # lean path
-    set -gx PATH $PATH $HOME/.elan/bin
-
-    # mount print
-    # set MNT /run/media/silver/
-
     # deepseek key
     set -l deep_key $(cat $HOME/Documents/keys/deepseek_key)
     export DEEPSEEK_API_KEY=$deep_key
@@ -35,23 +15,8 @@ if status is-interactive
     export BOOKMARKS_KEY=$(cat $HOME/Documents/keys/bookmarks_key)
 
     export BW_SESSION=$(cat $HOME/Documents/keys/bitwarden)
-    export LIBVIRT_DEFAULT_URI="qemu:///system"
 
     set -U fish_user_paths $HOME/.local/bin $fish_user_paths
-
-    export ANDROID_HOME=$HOME/.android_sdk
-    # export PATH=$PATH:$ANDROID_HOME/platform-tools
-    # export PATH=$PATH:$ANDROID_HOME/emulator
-    # export PATH=$PATH:$ANDROID_HOME/tools
-    # export PATH=$PATH:$ANDROID_HOME/tools/bin
-
-    # Initialize pyenv
-    # status --is-interactive; and pyenv init --path | source
-    # status --is-interactive; and pyenv init - | source
-    # status --is-interactive; and pyenv virtualenv-init - | source
-
-    # Allow deprecated scikit-learn package installation
-    set -gx SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL True
 
     function ra
         set tmp (mktemp -t "yazi-cwd.XXXXXX")
@@ -63,24 +28,6 @@ if status is-interactive
         rm -f "$tmp"
     end
 
-    # 开启系统代理
-    # set -x http_proxy http://127.0.0.1:7890
-    # set -x https_proxy http://127.0.0.1:7890
-    # set -x no_proxy 127.0.0.1,localhost
-    # set -x HTTP_PROXY http://127.0.0.1:7890
-    # set -x HTTPS_PROXY http://127.0.0.1:7890
-    # set -x NO_PROXY 127.0.0.1,localhost
-
-    # 关闭系统代理
-    # function proxy_off
-    #     set -e http_proxy
-    #     set -e https_proxy
-    #     set -e no_proxy
-    #     set -e HTTP_PROXY
-    #     set -e HTTPS_PROXY
-    #     set -e NO_PROXY
-    #     echo -e "\033[31m[×] 已关闭代理\033[0m"
-    # end
     alias ls="eza --icons"
     alias ll="eza --icons -l"
     alias la="eza --icons -a"
@@ -115,27 +62,10 @@ if status is-interactive
         end
     end
 
-    # 如果在使用 kitty, 则更改 ssh
     if test "$TERM" = xterm-kitty
         function ssh
             kitty +kitten ssh $argv
         end
-    end
-
-    function c
-        calc "2 / $argv * 10000"
-    end
-
-    function bg
-        eval "$argv & disown"
-    end
-
-    function cpr
-        rsync --archive -hh --partial --info=stats1,progress2 --modify-window=1 $argv
-    end
-
-    function mvr
-        rsync --archive -hh --partial --info=stats1,progress2 --modify-window=1 --remove-source-files $argv
     end
 
     set -gx EDITOR nvim
@@ -148,10 +78,6 @@ if status is-interactive
     set -g __fzf_fish_path $HOME/.fzf/bin
     set -gx PATH $PATH $HOME/.fzf/bin
 
-    # cuda
-    set -gx PATH $PATH /opt/cuda
-    export CUDA_HOME=/opt/cuda
-
     # FZF 绑定命令到 Ctrl-T 和 Ctrl-R
     function fzf-file-widget
         commandline -i (fzf)
@@ -160,24 +86,26 @@ if status is-interactive
         commandline -i (history | fzf)
     end
 
-    # vi mode
-    set -g fish_bind_mode fish_vi_key_bindings
+    bind ctrl-c cancel-commandline
 
-    # 绑定快捷键
-    function fish_user_key_bindings
-        bind -M insert \ct fzf-file-widget # Ctrl-T
-        bind -M insert \cr fzf-history-widget # Ctrl-R
-        # bind -M insert jk 'if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end'
-        bind -M insert \cf forward-char
-        bind -M insert \ca beginning-of-line
-        bind -M insert \ce end-of-line
-        bind -M default " "a beginning-of-line
-        bind -M default " "e end-of-line
-        bind -M default i forward-char
-        bind -M default l undo
-        bind -m insert u 'set fish_cursor_end_mode exclusive' repaint-mode
-        bind -M insert ctrl-c cancel-commandline
-        bind -M default e up-or-search
-        bind -M default n down-or-search
-    end
+    # # vi mode
+    # set -g fish_bind_mode fish_vi_key_bindings
+    #
+    # # 绑定快捷键
+    # function fish_user_key_bindings
+    #     bind -M insert \ct fzf-file-widget # Ctrl-T
+    #     bind -M insert \cr fzf-history-widget # Ctrl-R
+    #     # bind -M insert jk 'if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end'
+    #     bind -M insert \cf forward-char
+    #     bind -M insert \ca beginning-of-line
+    #     bind -M insert \ce end-of-line
+    #     bind -M default " "a beginning-of-line
+    #     bind -M default " "e end-of-line
+    #     bind -M default i forward-char
+    #     bind -M default l undo
+    #     bind -m insert u 'set fish_cursor_end_mode exclusive' repaint-mode
+    #     bind -M insert ctrl-c cancel-commandline
+    #     bind -M default e up-or-search
+    #     bind -M default n down-or-search
+    # end
 end
